21cf4fa227af77c88ce5d96514d5181dfb403af7
Q <q.ray@jbhunt.com>
2022-10-18 16:00:51 -0700

    setup connection between api and ui

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '@bg-hoard/store/ui-shared';
 import { RatingPipe } from '@bg-hoard/store/util-formatters';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Game } from '@bg-hoard/util-interface';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Component({
   standalone: true,
@@ -17,15 +19,25 @@ import { Game } from '@bg-hoard/util-interface';
     RatingPipe,
     HttpClientModule
   ],
+  providers: [
+    {
+      provide: 'baseUrl',
+      useValue: environment.apiUrl
+    }
+  ],
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Board Game Hoard';
-  games$ = this.http.get<Game[]>('/api/games');
+  games$!: Observable<Game[]>;
 
-  constructor(private readonly http: HttpClient) {
+  constructor(private readonly http: HttpClient, @Inject('baseUrl') private readonly baseUrl: string) {
     console.log('component constructed');
+  }
+
+  ngOnInit() {
+    this.games$ = this.http.get<Game[]>(`${this.baseUrl}/api/games`);
   }
 }
--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatCardModule } from "@angular/material/card";
 import { ActivatedRoute, ParamMap } from "@angular/router";
@@ -6,19 +6,30 @@ import { map, switchMap } from "rxjs";
 import { HttpClient } from '@angular/common/http';
 import { RatingPipe } from '@bg-hoard/store/util-formatters';
 import { MatButtonModule } from '@angular/material/button';
+import { environment } from '../../../../../../apps/store/src/environments/environment';
 
 @Component({
   selector: 'bg-hoard-game-detail',
   standalone: true,
+  providers: [
+    {
+      provide: 'baseUrl',
+      useValue: environment.apiUrl
+    }
+  ],
   imports: [CommonModule, MatCardModule, RatingPipe, MatButtonModule],
   templateUrl: './game-detail.component.html',
   styleUrls: ['./game-detail.component.css'],
 })
 export class GameDetailComponent {
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(
+      private route: ActivatedRoute,
+      private http: HttpClient,
+      @Inject('baseUrl') private readonly baseUrl: string
+    ) {}
 
   game$ = this.route.paramMap.pipe(
     map((params: ParamMap) => params.get('id')),
-    switchMap(id => this.http.get<any>(`/api/games/${id}`))
+    switchMap(id => this.http.get<any>(`${this.baseUrl}/api/games/${id}`))
   );
 }
